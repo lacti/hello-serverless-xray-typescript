7e43fa2344555dfc33f1438b44d69b93150eb584
jaeyoung.choi <lactrious@gmail.com>
2019-09-30 08:19:18 +0900

    Install dummy capture for testing

--- handler.ts
+++ handler.ts
@@ -1,18 +1,25 @@
 import { APIGatewayProxyHandler } from "aws-lambda";
 import "source-map-support/register";
-import "aws-xray-sdk-core";
+import { captureAsyncFunc } from "aws-xray-sdk-core";
 
-export const hello: APIGatewayProxyHandler = async (event, _context) => {
+const sleep = (millis: number) =>
+  captureAsyncFunc(
+    "sleep",
+    seg =>
+      new Promise<void>(resolve =>
+        setTimeout(() => {
+          resolve();
+          seg.close();
+        }, millis)
+      )
+  );
+
+export const hello: APIGatewayProxyHandler = async () => {
+  for (let i = 0; i < 10; ++i) {
+    await sleep(100);
+  }
   return {
     statusCode: 200,
-    body: JSON.stringify(
-      {
-        message:
-          "Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!",
-        input: event
-      },
-      null,
-      2
-    )
+    body: "OK"
   };
 };
